95f1026462a0ab46e074800a5949164503ac445b
pablo-pacheco <pablo.pacheco@origgami.com.br>
2017-04-28 18:38:23 -0300

    Add underscore to postmetas

--- classes/marketplace/class-alg-mpwc-post-metas.php
+++ classes/marketplace/class-alg-mpwc-post-metas.php
@@ -11,13 +11,13 @@ if ( ! class_exists( 'Alg_MPWC_Post_Metas' ) ) {
 	class Alg_MPWC_Post_Metas {
 
 		// Comission Meta fields
-		const COMMISSION_PRODUCT_IDS = 'alg_mpwc_product_ids';
-		const COMMISSION_AUTHOR_ID   = 'alg_mpwc_author_id';
-		const COMMISSION_ORDER_ID    = 'alg_mpwc_order_id';
-		const COMMISSION_VALUE       = 'alg_mpwc_comission_value';
-		const COMMISSION_STATUS      = 'alg_mpwc_status';
+		const COMMISSION_PRODUCT_IDS = '_alg_mpwc_product_ids';
+		const COMMISSION_AUTHOR_ID   = '_alg_mpwc_author_id';
+		const COMMISSION_ORDER_ID    = '_alg_mpwc_order_id';
+		const COMMISSION_VALUE       = '_alg_mpwc_comission_value';
+		const COMMISSION_STATUS      = '_alg_mpwc_status';
 
 		// Order Meta fields
-		const ORDER_COMISSIONS_EVALUATED = 'alg_mpwc_comissions_evaluated';
+		const ORDER_COMISSIONS_EVALUATED = '_alg_mpwc_comissions_evaluated';
 	}
 }
