80500695f4d3716ca317eadca8fad19df986a5f9
Pablo S G Pacheco <pablo.sg.pacheco@gmail.com>
2018-04-25 17:40:55 -0300

    Update version

--- marketplace-for-woocommerce.php
+++ marketplace-for-woocommerce.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Marketplace for WooCommerce
 Description: Let users sell on your store
-Version: 1.1.8
+Version: 1.1.9
 Author: Algoritmika Ltd
 Author URI: http://algoritmika.com
 Copyright: © 2017 Algoritmika Ltd.
