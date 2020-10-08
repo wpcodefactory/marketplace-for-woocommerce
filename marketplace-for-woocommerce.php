a2f8f83642d8ffeab2dc791600aafb367a87f9df
algoritmika <info@algoritmika.com>
2020-10-08 15:47:31 +0300

    v1.3.3

--- marketplace-for-woocommerce.php
+++ marketplace-for-woocommerce.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Marketplace for WooCommerce
 Description: Let users sell on your store.
-Version: 1.3.3-dev
+Version: 1.3.3
 Author: Algoritmika Ltd
 Author URI: https://algoritmika.com
 Copyright: © 2020 Algoritmika Ltd.
