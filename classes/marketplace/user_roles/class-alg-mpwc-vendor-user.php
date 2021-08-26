1345d48b2e1da08dd73ecff45c60bc95a822d85c
algoritmika <info@algoritmika.com>
2021-08-27 01:09:34 +0300

    v1.4.2

--- classes/marketplace/user_roles/class-alg-mpwc-vendor-user.php
+++ classes/marketplace/user_roles/class-alg-mpwc-vendor-user.php
@@ -10,168 +10,168 @@
 
 if ( ! class_exists( 'Alg_MPWC_Vendor_User' ) ) :
 
-	class Alg_MPWC_Vendor_User {
+class Alg_MPWC_Vendor_User {
 
-		/**
-		 * Constructor.
-		 *
-		 * @version 1.0.0
-		 * @since   1.0.0
-		 */
-		function __construct() {
+	/**
+	 * Constructor.
+	 *
+	 * @version 1.0.0
+	 * @since   1.0.0
+	 */
+	function __construct() {
 
-			// Manages the public page of the vendor user
-			new Alg_MPWC_Vendor_Public_Page();
+		// Manages the public page of the vendor user
+		new Alg_MPWC_Vendor_Public_Page();
 
-			// Manages vendor user (Create role, manages access)
-			$vendor_role = new Alg_MPWC_Vendor_Role();
-			$vendor_role->init();
+		// Manages vendor user (Create role, manages access)
+		$vendor_role = new Alg_MPWC_Vendor_Role();
+		$vendor_role->init();
 
-			// Creates vendor admin fields
-			add_action( 'cmb2_init', array( $this, 'add_admin_fields' ) );
+		// Creates vendor admin fields
+		add_action( 'cmb2_init', array( $this, 'add_admin_fields' ) );
 
-			// Setups the block vendor option
-			new Alg_MPWC_Vendor_Block_Option();
+		// Setups the block vendor option
+		new Alg_MPWC_Vendor_Block_Option();
 
-			// Setups vendor registry
-			new Alg_MPWC_Vendor_Registry();
+		// Setups vendor registry
+		new Alg_MPWC_Vendor_Registry();
 
-			// Setups marketplace tab on my account page
-			new Alg_MPWC_Vendor_Marketplace_Tab();
+		// Setups marketplace tab on my account page
+		new Alg_MPWC_Vendor_Marketplace_Tab();
 
-			// Adds (By vendor name) on product loop
-			add_action( 'woocommerce_after_shop_loop_item', array( $this, 'display_product_author_on_loop' ), 9 );
+		// Adds (By vendor name) on product loop
+		add_action( 'woocommerce_after_shop_loop_item', array( $this, 'display_product_author_on_loop' ), 9 );
 
-			// Product tab
-			add_filter( 'woocommerce_product_tabs', array( $this, 'add_tab_on_product' ) );
+		// Product tab
+		add_filter( 'woocommerce_product_tabs', array( $this, 'add_tab_on_product' ) );
 
-			// Add pending vendor email class
-			add_filter( 'woocommerce_email_classes', array( $this, 'add_pending_vendor_email' ) );
-		}
+		// Add pending vendor email class
+		add_filter( 'woocommerce_email_classes', array( $this, 'add_pending_vendor_email' ) );
+	}
 
-		/**
-		 * Adds pending vendor email class.
-		 *
-		 * @version 1.0.0
-		 * @since   1.0.0
-		 */
-		public function add_pending_vendor_email( $email_classes ) {
-			$email_classes['WC_Vendor_Registration_Email'] = new Alg_MPWC_Vendor_Registration_Email();
-			return $email_classes;
-		}
+	/**
+	 * Adds pending vendor email class.
+	 *
+	 * @version 1.0.0
+	 * @since   1.0.0
+	 */
+	public function add_pending_vendor_email( $email_classes ) {
+		$email_classes['WC_Vendor_Registration_Email'] = new Alg_MPWC_Vendor_Registration_Email();
+		return $email_classes;
+	}
 
-		/**
-		 * Create a tab on product page displaying some info about the vendor.
-		 *
-		 * @version 1.0.2
-		 * @since   1.0.0
-		 */
-		public function add_tab_on_product( $tabs ) {
-			global $post;
-			$user                = get_user_by( 'ID', $post->post_author );
-			$product_tab_enabled = filter_var( get_option( Alg_MPWC_Settings_Vendor::OPTION_PRODUCT_TAB_ENABLE, true ), FILTER_VALIDATE_BOOLEAN );
+	/**
+	 * Create a tab on product page displaying some info about the vendor.
+	 *
+	 * @version 1.0.2
+	 * @since   1.0.0
+	 */
+	public function add_tab_on_product( $tabs ) {
+		global $post;
+		$user                = get_user_by( 'ID', $post->post_author );
+		$product_tab_enabled = filter_var( get_option( Alg_MPWC_Settings_Vendor::OPTION_PRODUCT_TAB_ENABLE, true ), FILTER_VALIDATE_BOOLEAN );
 
-			if (
-				! $product_tab_enabled ||
-				! $user ||
-				! in_array( Alg_MPWC_Vendor_Role::ROLE_VENDOR, $user->roles )
-			) {
-				return $tabs;
-			}
-
-			$product_tab_priority = filter_var( get_option( Alg_MPWC_Settings_Vendor::OPTION_PRODUCT_TAB_PRIORITY, 40 ), FILTER_SANITIZE_NUMBER_INT );
-			$product_tab_text     = sanitize_text_field( get_option( Alg_MPWC_Settings_Vendor::OPTION_PRODUCT_TAB_TEXT, __( 'Vendor', 'marketplace-for-woocommerce' )));
-
-			$tabs['alg_mpwc_vendor'] = array(
-				'title'    => $product_tab_text,
-				'priority' => $product_tab_priority,
-				'callback' => array( $this, 'create_vendor_tab_template' ),
-			);
-
+		if (
+			! $product_tab_enabled ||
+			! $user ||
+			! in_array( Alg_MPWC_Vendor_Role::ROLE_VENDOR, $user->roles )
+		) {
 			return $tabs;
 		}
 
-		/**
-		 * The html for vendor tab on product.
-		 *
-		 * @version 1.4.2
-		 * @since   1.0.0
-		 *
-		 * @todo    [now] (feature) customizable content || rating: add options: enable/disable, content
-		 * @todo    [now] (dev) class `alg_mpwc_vendor_rating` to `alg-mpwc-vendor-rating`
-		 */
-		public function create_vendor_tab_template() {
-			global $post;
+		$product_tab_priority = filter_var( get_option( Alg_MPWC_Settings_Vendor::OPTION_PRODUCT_TAB_PRIORITY, 40 ), FILTER_SANITIZE_NUMBER_INT );
+		$product_tab_text     = sanitize_text_field( get_option( Alg_MPWC_Settings_Vendor::OPTION_PRODUCT_TAB_TEXT, __( 'Vendor', 'marketplace-for-woocommerce' )));
 
-			// User
-			$user = get_user_by( 'ID', $post->post_author );
+		$tabs['alg_mpwc_vendor'] = array(
+			'title'    => $product_tab_text,
+			'priority' => $product_tab_priority,
+			'callback' => array( $this, 'create_vendor_tab_template' ),
+		);
 
-			// Public page url
-			$user_public_page_url = Alg_MPWC_Vendor_Public_Page::get_public_page_url( $post->post_author );
+		return $tabs;
+	}
 
-			// User fields
-			$fields = new Alg_MPWC_Vendor_Admin_Fields();
+	/**
+	 * The html for vendor tab on product.
+	 *
+	 * @version 1.4.2
+	 * @since   1.0.0
+	 *
+	 * @todo    [now] (feature) customizable content || rating: add options: enable/disable, content
+	 * @todo    [now] (dev) class `alg_mpwc_vendor_rating` to `alg-mpwc-vendor-rating`
+	 */
+	public function create_vendor_tab_template() {
+		global $post;
 
-			// Logo
-			$logo_id = filter_var( get_user_meta( $user->ID, $fields->meta_logo . '_id', true ), FILTER_VALIDATE_INT );
-			if ( $logo_id ) {
-				$image = wp_get_attachment_image( $logo_id, 'full', false, array( 'style' => 'max-width:38%;float:left;margin:0 15px 0 0' ) );
-				echo '<a href="' . $user_public_page_url . '">' . $image . '</a>';
-			}
+		// User
+		$user = get_user_by( 'ID', $post->post_author );
 
-			// Title
-			$store_title = sanitize_text_field( get_user_meta( $user->ID, $fields->meta_store_title, true ) );
-			$title = $store_title ? $store_title : $user->display_name;
-			echo '<h2 style="display:inline">' . $title . '</h2>';
+		// Public page url
+		$user_public_page_url = Alg_MPWC_Vendor_Public_Page::get_public_page_url( $post->post_author );
 
-			// Description
-			$description = sanitize_text_field( get_user_meta( $user->ID, $fields->meta_description, true ) );
-			echo $description ? apply_filters( 'the_content', $description ) : '';
+		// User fields
+		$fields = new Alg_MPWC_Vendor_Admin_Fields();
 
-			// See all products
-			echo '<div class="alg-mpwc-product-author"><a href="' . $user_public_page_url . '">' . sprintf( __( 'Go to %s', 'marketplace-for-woocommerce' ), $title ) . '</a></div>';
-
+		// Logo
+		$logo_id = filter_var( get_user_meta( $user->ID, $fields->meta_logo . '_id', true ), FILTER_VALIDATE_INT );
+		if ( $logo_id ) {
+			$image = wp_get_attachment_image( $logo_id, 'full', false, array( 'style' => 'max-width:38%;float:left;margin:0 15px 0 0' ) );
+			echo '<a href="' . $user_public_page_url . '">' . $image . '</a>';
 		}
 
-		/**
-		 * Displays the product's author on product loop.
-		 *
-		 * @version 1.0.2
-		 * @since   1.0.0
-		 */
-		public function display_product_author_on_loop() {
-			global $post;
-			$user = get_user_by( 'ID', $post->post_author );
-			$authorship_link_enabled = filter_var( get_option( Alg_MPWC_Settings_Vendor::OPTION_AUTHORSHIP_PRODUCT_LOOP, true ), FILTER_VALIDATE_BOOLEAN );
+		// Title
+		$store_title = sanitize_text_field( get_user_meta( $user->ID, $fields->meta_store_title, true ) );
+		$title = $store_title ? $store_title : $user->display_name;
+		echo '<h2 style="display:inline">' . $title . '</h2>';
 
-			if (
-				! $user ||
-				! in_array( Alg_MPWC_Vendor_Role::ROLE_VENDOR, $user->roles ) ||
-				! $authorship_link_enabled
-			) {
-				return;
-			}
+		// Description
+		$description = sanitize_text_field( get_user_meta( $user->ID, $fields->meta_description, true ) );
+		echo $description ? apply_filters( 'the_content', $description ) : '';
 
-			$fields               = new Alg_MPWC_Vendor_Admin_Fields();
-			$user_public_page_url = Alg_MPWC_Vendor_Public_Page::get_public_page_url( $post->post_author );
-			$store_title          = sanitize_text_field( get_user_meta( $user->ID, $fields->meta_store_title, true ) );
-			$title                = $store_title ? $store_title : $user->display_name;
-
-			echo '<div class="alg-mpwc-product-author"><a href="' . $user_public_page_url . '">By ' . $title . '</a></div>';
-		}
-
-		/**
-		 * Creates vendor admin fields.
-		 *
-		 * @version 1.0.0
-		 * @since   1.0.0
-		 */
-		public function add_admin_fields() {
-			$admin_fields = new Alg_MPWC_Vendor_Admin_Fields();
-			$admin_fields->add_fields();
-			$admin_fields->setup_custom_css();
-		}
+		// See all products
+		echo '<div class="alg-mpwc-product-author"><a href="' . $user_public_page_url . '">' . sprintf( __( 'Go to %s', 'marketplace-for-woocommerce' ), $title ) . '</a></div>';
 
 	}
+
+	/**
+	 * Displays the product's author on product loop.
+	 *
+	 * @version 1.0.2
+	 * @since   1.0.0
+	 */
+	public function display_product_author_on_loop() {
+		global $post;
+		$user = get_user_by( 'ID', $post->post_author );
+		$authorship_link_enabled = filter_var( get_option( Alg_MPWC_Settings_Vendor::OPTION_AUTHORSHIP_PRODUCT_LOOP, true ), FILTER_VALIDATE_BOOLEAN );
+
+		if (
+			! $user ||
+			! in_array( Alg_MPWC_Vendor_Role::ROLE_VENDOR, $user->roles ) ||
+			! $authorship_link_enabled
+		) {
+			return;
+		}
+
+		$fields               = new Alg_MPWC_Vendor_Admin_Fields();
+		$user_public_page_url = Alg_MPWC_Vendor_Public_Page::get_public_page_url( $post->post_author );
+		$store_title          = sanitize_text_field( get_user_meta( $user->ID, $fields->meta_store_title, true ) );
+		$title                = $store_title ? $store_title : $user->display_name;
+
+		echo '<div class="alg-mpwc-product-author"><a href="' . $user_public_page_url . '">By ' . $title . '</a></div>';
+	}
+
+	/**
+	 * Creates vendor admin fields.
+	 *
+	 * @version 1.0.0
+	 * @since   1.0.0
+	 */
+	public function add_admin_fields() {
+		$admin_fields = new Alg_MPWC_Vendor_Admin_Fields();
+		$admin_fields->add_fields();
+		$admin_fields->setup_custom_css();
+	}
+
+}
 
 endif;
