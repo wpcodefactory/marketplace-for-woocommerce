3761bd98439bba8cf4d74db74bc8dd806e812273
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2021-10-26 11:43:13 -0300

    Fix version

--- marketplace-for-woocommerce.php
+++ marketplace-for-woocommerce.php
@@ -3,7 +3,7 @@
 Plugin Name: Marketplace for WooCommerce
 Plugin URI: https://wordpress.org/plugins/marketplace-for-woocommerce/
 Description: Let users sell on your store.
-Version: 1.4.6
+Version: 1.4.6-dev
 Author: Algoritmika Ltd
 Author URI: https://algoritmika.com
 Text Domain: marketplace-for-woocommerce
@@ -14,7 +14,7 @@ WC tested up to: 5.8
 
 defined( 'ABSPATH' ) || exit;
 
-defined( 'ALG_WC_MARKETPLACE_VERSION' ) || define( 'ALG_WC_MARKETPLACE_VERSION', '1.4.5' );
+defined( 'ALG_WC_MARKETPLACE_VERSION' ) || define( 'ALG_WC_MARKETPLACE_VERSION', '1.4.6-dev-20211026-1142' );
 
 /**
  * Check for active plugins.
